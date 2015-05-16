9da1fd27873a3202d5ab037a655467e734cb955d
maximusiis <tricersalophus@aol.com>
2015-05-16 18:21:14 -0400

    Update player.js

--- player.js
+++ player.js
@@ -32,20 +32,52 @@ function player(){
 	{
 		return this.hp <= 0;
 	}
+	function getxPos()
+	{
+		return this.xpos;
+	}
+	function getyPos()
+	{
+		return this.ypos;
+	}
 	function move()
 	{
 		var code = e.keyCode ? e.keyCode : e.which;
 		if (code == 37) //left
 			this.xpos = this.xpos - this.speed;
-		if (code == 38)
+		else if (code == 38) //up
 			this.ypos = this.ypos + this.speed;
-		if (code == 39)
+		else if (code == 39) //right
 			this.xpos = this.xpos + this.speed;
-		if (code == 40)
+		else if (code == 40) //down
 			this.ypos = this.ypos - this.speed;
+		return code;
 	}
 	function shoot()
 	{
-		//to be implemented
+		var bullet = bullet(atk, speed, move());
+	}
+}
+function bullet(atk, speed, direct)
+{
+	this.atk = atk;
+	this.speed = speed;
+	this.direction = direct;
+	this.xpos = player().getxPos();
+	this.ypos = player().getyPos();
+	function move()
+	{
+		if (code == 37) //left
+			this.xpos = this.xpos - this.speed;
+		if (code == 38) //up
+			this.ypos = this.ypos - this.speed;
+		if (code == 39) //right
+			this.xpos = this.xpos - this.speed;
+		if (code == 40) //down
+			this.ypos = this.ypos - this.speed;
+	}
+	function getAtk()
+	{
+		return this.atk;
 	}
 }
