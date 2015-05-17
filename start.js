421f66df528ed340ca76a4d9b183e96de278ae34
cyncyn <cyncyn.zhuang@gmail.com>
2015-05-16 21:07:00 -0400

    Update start.js
    added dmg parameter to generating of Genome

--- start.js
+++ start.js
@@ -1,9 +1,11 @@
 		var points = 0;
 		var enemies = [];
 		var numEnemies = 20;
+		var level = 1;
 		function start()
 		{
-			window.setInterval(check, 3000);
+			while(true)
+			window.setInterval(check, 100);
 
 			for(var i = 0; i < numEnemies; i++)
 			{
@@ -11,7 +13,7 @@
 				enemies.push(generateEnemy(1, 1));
 			}
 		}
-		function generateEnemy(mhp, sp)
+		function generateEnemy(mhp, sp, dmg)
 		{
 			if(mhp < 5)
 				var hp = Math.floor((Math.random()*(mhp+5)) + 1);
@@ -19,26 +21,35 @@
 				var hp = Math.floor((Math.random()*(mhp+5)) + (mhp-5));
 
 			if(sp < 5)
-				var speed = Math.floor((Math.random()*(mhp+5)) + 1);
+				var speed = Math.floor((Math.random()*(sp+5)) + 1);
 			else
-				var speed = Math.floor((Math.random()*(mhp+5)) + (mhp-5));
+				var speed = Math.floor((Math.random()*(sp+5)) + (sp-5));
+
+			if(sp < 5)
+				var damage = Math.floor((Math.random()*(damage+5)) + 1);
+			else
+				var damage = Math.floor((Math.random()*(damage+5)) + (damage-5));
 
 			var mov = generateMov();
-			var atk = generateAtk();
+
+			if(level == 1)
+				var atk = generateAtk(false, );
+
 			var xPos = 1; //leaving it as constant for now 
 			var yPos = 1; //leaving it as contant for now
 
-			var e = new Enemy(new Genome(hp, speed, mov, atk), xPos, yPos);
+			var e = new Enemy(new Genome(hp, speed, mov, atk, damage), xPos, yPos);
+
+		}
+		function generateAtk(can, dmg, num, spd)
+		{
+			
+			var atk = new AttackPattern(can, dmg, num, spd)
 
 
 		}
-		function generateAtk(can,dmg,num,spd)
+		function generateMov(can, comseq)
 		{
-
-
-		}
-		function generateMov()
-		{
-
+			//to be implemented
 		}
 	
